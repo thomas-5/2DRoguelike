c0c57cd304563354b771884e77afa8fe8dcbed3f
ziyue.thomas.lin@gmail.com <ziyue.thomas.lin@gmail.com>
2025-09-20 15:05:05 -0400

    Add UI for food amount tracking

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
@@ -6,12 +7,14 @@ public class GameManager : MonoBehaviour
     public BoardManager BoardManager;
     public PlayerController PlayerController;
     public TurnManager TurnManager { get; private set; }
-
+    public UIDocument UIDoc;
+    private Label m_FoodLabel;
     private int m_FoodAmount = 100;
 
     private void OnTurnHappen()
     {
         m_FoodAmount -= 1;
+        m_FoodLabel.text = "Food : " + m_FoodAmount;
         Debug.Log("Current amount of food: " + m_FoodAmount);
     }
 
@@ -27,6 +30,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
+        m_FoodLabel.text = "Food : " + m_FoodAmount;
         TurnManager = new TurnManager();
         TurnManager.OnTick += OnTurnHappen;
 
