2c082bd89861868991929c13fc248dfbd952cabb
ziyue.thomas.lin@gmail.com <ziyue.thomas.lin@gmail.com>
2025-09-20 15:17:47 -0400

    Generate food on empty cells

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour
 {
     public class CellData
     {
         public bool Passable;
+        public GameObject ContainedObject;
     }
 
     private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
+    private List<Vector2Int> m_EmptyCellsList;
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
+    public GameObject FoodPrefab;
 
     public Vector3 CellToWorld(Vector2Int cellIndex)
     {
@@ -35,22 +39,38 @@ public class BoardManager : MonoBehaviour
     // Start is called before the first frame update
     public PlayerController Player;
 
+    void GenerateFood()
+    {
+        int foodCount = 5;
+        for (int i = 0; i < foodCount; ++i)
+        {
+            int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
+            Vector2Int coord = m_EmptyCellsList[randomIndex];
+            
+            m_EmptyCellsList.RemoveAt(randomIndex);
+            CellData data = m_BoardData[coord.x, coord.y];
+            GameObject newFood = Instantiate(FoodPrefab);
+            newFood.transform.position = CellToWorld(coord);
+            data.ContainedObject = newFood;
+        }
+    }
+
     // Start is called before the first frame update
     public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
         m_Grid = GetComponentInChildren<Grid>();
-        
         m_BoardData = new CellData[Width, Height];
+        m_EmptyCellsList = new List<Vector2Int>();
 
         for (int y = 0; y < Height; ++y)
         {
-            for(int x = 0; x < Width; ++x)
+            for (int x = 0; x < Width; ++x)
             {
                 Tile tile;
                 m_BoardData[x, y] = new CellData();
 
-                if(x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
+                if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
                     m_BoardData[x, y].Passable = false;
@@ -59,10 +79,17 @@ public class BoardManager : MonoBehaviour
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
                     m_BoardData[x, y].Passable = true;
+
+                    //this is a passable empty cell, add it to the list!
+                    m_EmptyCellsList.Add(new Vector2Int(x, y));
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
+
+        // remove the starting point of the player
+        m_EmptyCellsList.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 }
